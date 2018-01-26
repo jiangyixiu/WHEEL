3df2aada97b0ee40bda68e2fd40b036d417b74ee
jiangyixiu <2227006164@qq.com>
2018-01-26 14:08:00 +0800

    s

--- H5/AppWeb/teamNew/js/exam.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by jiangyixiu on 2017/5/31.
- */
-'use strict';
-var App = angular.module('app', []);
-
-App.controller('phoneTypeCtr', ['$http', '$state', '$scope', function ($http, $state, $scope) {
-
-}]);
--- H5/webApp/app/readme.md
+++
@@ -1,2 +0,0 @@
-好多课APP内使用页面
-	courseContent.html
